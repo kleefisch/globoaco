1beef765ca0be58da4a7d39e5644401f50e9227d
kleefisch <johnatan.f@hotmail.com>
2026-07-01 18:13:28 +0200

    fix: pin sanity studio runtime

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,10 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
+    appId: 'sjvst2fnsis7aumhxwyggdpi',
+    autoUpdates: false,
   }
 })
